b5fa04db6acc5177388996480fa374ed0bda69bf
Seniru <senirupasan@gmail.com>
2025-11-17 19:25:55 +0530

    nginx reverse proxy

---
+++ nginx_practice/server.js
@@ -0,0 +1,19 @@
+const express = require("express")
+const app = express()
+
+app.use((req, res, next) => {
+    console.log(req.method, req.path)
+    next()
+})
+
+app.get("/", (req, res, next) => {
+    res.json({
+        message: "Hello world"
+    })
+})
+
+
+app.listen(3000, (err) => {
+    if (err) console.error(err)
+    console.log("Listening on port 3000")
+})
