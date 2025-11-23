e8cc3314875ba0cdb0b541a5da4663d0a3208d71
Seniru <senirupasan@gmail.com>
2025-11-23 10:09:50 +0530

    easy challenges

---
+++ qualifier/archive-archaeologist/execute.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+mkdir -p out
+
+shopt -s nullglob  
+while true; do
+    files=(src/*.gz)
+    file=${files[0]}
+    echo ${#files[@]}
+    echo $file
+    if [[ -z $file ]]; then
+        break
+    fi
+    echo unarchiving $file
+    tar -xvzf "$file" -C "src"
+    rm $file
+done
+
+file=src/*.txt
+mv $file out
